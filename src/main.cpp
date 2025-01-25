2a98651e9a61c931a2a186f58db0bc9a2c63b3b9
Nate Stott <nate.stott@pm.me>
2025-01-25 11:53:59 -0500

    exit command 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 
-[[noreturn]] int main() {
+int main() {
     // Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    while (true) {
-        std::cout << "$ ";
+    for(;;) {
 
+        std::cout << "$ ";
         std::string input;
         std::getline(std::cin, input);
 
-        std::cout << input << ": command not found" << std::endl;
+        if(input == "exit 0") return 0;
+
+        std::cout << input << ": command not found\n";
     }
 }
